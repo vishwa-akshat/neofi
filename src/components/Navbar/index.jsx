13a0f51fb01c94413967adaa48ba07d1f2f6615b
vishwa-akshat <akvs0707@gmail.com>
2023-05-01 08:08:25 +0530

    feat: nav links active styles added

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,58 @@
+import { useState } from "react";
+
 import Logo from "../../assets/logo.svg";
 
 import "./style.scss";
 
 export default function Navbar() {
+    const [activeLink, setActiveLink] = useState("trade");
+
+    const handleLinkActive = (event) => {
+        setActiveLink(event.target.name);
+    };
+
     return (
         <div className="navbar-wrapper">
             <div className="logo">
                 <img src={Logo} alt="neofi" />
             </div>
             <div className="links-wrapper">
-                <button className="nav-links">Trade</button>
-                <button className="nav-links">Earn</button>
-                <button className="nav-links">Support</button>
-                <button className="nav-links">About</button>
+                <button
+                    className={`nav-links ${
+                        activeLink === "trade" && "active-nav-link"
+                    }`}
+                    onClick={handleLinkActive}
+                    name="trade"
+                >
+                    Trade
+                </button>
+                <button
+                    className={`nav-links ${
+                        activeLink === "earn" && "active-nav-link"
+                    }`}
+                    onClick={handleLinkActive}
+                    name="earn"
+                >
+                    Earn
+                </button>
+                <button
+                    className={`nav-links ${
+                        activeLink === "support" && "active-nav-link"
+                    }`}
+                    onClick={handleLinkActive}
+                    name="support"
+                >
+                    Support
+                </button>
+                <button
+                    className={`nav-links ${
+                        activeLink === "about" && "active-nav-link"
+                    }`}
+                    onClick={handleLinkActive}
+                    name="about"
+                >
+                    About
+                </button>
             </div>
             <button className="connect-wallet-btn">Connect wallet</button>
         </div>
